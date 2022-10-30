ff5eb35977a316c2f651fb746c39508d4ed6bcbe
Matheus Alves <matheusalves.bmd@gmail.com>
2022-10-29 22:11:24 -0300

    Provide a provisory solution for the challenge

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import game from "./data.js";
+
+const { players } = game;
+
+// 1. Create one player array for each team
+const [players1, players2] = players;
+
+// 2. For team 1, create a variable with the goal keeper and and array with the rest
+const [gk, ...fieldPlayers] = players1;
+
+// 3. Array with all players from both teams
+const allPlayers = [...players1, ...players2];
+
+// 4. Create the players1Final array
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+// 5. Create one variable for each odd
+const { odds } = game;
+const { team1, x: draw, team2 } = odds;
+
+// 6. Write a function
+const printGoals = (...names) => {
+  let totalGoals = 0;
+  for (const name of names) {
+    totalGoals++;
+    console.log(name);
+  }
+  console.log(totalGoals);
+};
+
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+
+const { scored } = game;
+
+printGoals(...scored);
+
+// 7. Print the team that is more likely to win
+console.log((team1 > team2 && "team2") || "team1");
