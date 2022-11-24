009a03385d1d735c8a46aaaccfd7d9c0788a3c37
Matheus Alves <matheusalves.bmd@gmail.com>
2022-11-24 17:47:43 -0300

    Add the data for the Coding Challenge #3

--- src/data.js
+++ src/data.js
@@ -15,4 +15,18 @@ const game = {
   },
 };
 
-export default game;
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+export { game, gameEvents };
